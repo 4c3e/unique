8588d16ada9a90db2783bc0ba684553a503ff0e6
4ce <4ce@i2pmail.org>
2022-01-24 19:19:05 -0500

    hello world termbox app

---
+++ README.md
@@ -0,0 +1,2 @@
+# unique
+Unique is a Unicode Art Editor that runs in the terminal. Best paired with the [Unscii](http://viznut.fi/unscii/) font.
---
+++ src/hello.c
@@ -0,0 +1,37 @@
+#include <unistd.h> // for sleep()
+#define TB_OPT_TRUECOLOR 1
+#define TB_IMPL
+#include "termbox.h"
+
+
+int main(int argc, char **argv) {
+  if (tb_init() != 0)
+    return 1; // couldn't initialize our screen
+
+  tb_set_input_mode(5);
+  // set up our colors
+  uint32_t bg = 0x88B8A0;
+  uint32_t fg = 0xa281c6;
+
+  // get the screen resolution
+  int w = tb_width();
+  int h = tb_height();
+
+  // calculate the coordinates for the center of the screen
+  int x = (w/2);
+  int y = (h/2);
+
+  // put some text into those coordinates
+  tb_set_cell(x-4, y, 'h', fg, bg);
+
+  // flush the output to the screen
+  tb_present();
+
+  // wait a few secs
+  sleep(3);
+
+  // and finally, shutdown the screen so we exit cleanly
+  tb_shutdown();
+
+  return 0;
+}
